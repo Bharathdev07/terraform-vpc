7edecd3af64575cb7dac269b53f8d9549d2caeff
Bharathdev07 <devops854@gmail.com>
2024-08-09 00:08:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -89,3 +89,46 @@ resource "aws_subnet" "private" {
     Name = "private-subnet-${count.index}"
   }
 }
+resource "aws_security_group" "public" {
+  vpc_id = aws_vpc.main.id
+  name   = var.public_sg_name
+  tags = {
+    Name = var.public_sg_name
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "private" {
+  vpc_id = aws_vpc.main.id
+  name   = var.private_sg_name
+  tags = {
+    Name = var.private_sg_name
+  }
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["10.0.0.0/8"]  # Adjust this as needed
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
